6bbbf58a48d27e7801b6090d0f42f4bcfca7aae1
mtagizadehbioinf <mtaghizadeh@alumni.ut.ac.ir>
2023-01-26 21:36:22 +0330

    sd

--- R/main.R
+++ R/main.R
@@ -2,7 +2,8 @@
 
 ## pascal triangle as a matrix
 
-ptmatrix <- function(ptfn){
+ptmatrix <- function(ptfn = 5){
+  cat("default parameters: ptfn = 5", '\n', '\n')
   ptfmat <- matrix(0, ptfn+1, (ptfn*2)+1)
   ptfmat[1, ptfn+1] <- 1
   for (i in 1:(ptfn-1)) {
@@ -18,12 +19,14 @@ ptmatrix <- function(ptfn){
 
 
 ## Short R code for pascal triangle
-short_code_pascal_triangle <- function(pt){
+short_code_pascal_triangle <- function(pt = 5){
+  cat("default parameters: pt = 5", '\n', '\n')
   lapply(0:pt, FUN = function(x)choose(x, 0:x))
 }
 
 
-fibonacci <- function(fa, fb){
+fibonacci <- function(fa = 1, fb = 8){
+  cat("default parameters: fa = 1, fb = 8", '\n', '\n')
   x <- c(1,1)
   for (i in 1:fb) {
     x[length(x)+1] <- sum(x[length(x)-1], x[length(x)])
@@ -32,12 +35,12 @@ fibonacci <- function(fa, fb){
 }
 
 
-fibonacci_2D <- function(rf2D, cf2D){
+fibonacci_2D <- function(rf2D = 4, cf2D = 5){
+  cat("default parameters: rf2D = 4, cf2D = 5", '\n', '\n')
   a <- 1
   b <- cf2D
   c <- rf2D
   d <- (b-a)+c
   mapply(a:b, c:d, FUN = function(x,y)fibonacci(x,y))
 }
-
 
